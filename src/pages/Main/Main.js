e2ac7c506e9d4798d7331105056cb747181ab997
unknown <chkevi@gmail.com>
2022-03-29 17:52:27 +0900

    Add: 메인 페이지 작업 시작

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './Main.scss';
 
 const Main = () => {
-  return <div>wesop</div>;
+  return (
+    <div className="Main">
+      <div className="MainVideo">a</div>
+      <div className="MainFirstSlide">a</div>
+      <div className="MainVitamin">a</div>
+      <div className="MainShampooIntro">a</div>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+    </div>
+  );
 };
 
 export default Main;
