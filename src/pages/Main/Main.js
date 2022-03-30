24cffe2524ad137956ce7f06bf8ee339b9fd4a79
unknown <chkevi@gmail.com>
2022-03-30 21:56:50 +0900

    Add: 슬라이드 레이아웃 완성

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import MainVideo from './MainVideo';
 import MainCarousel from './MainCarousel';
-import SlideData from './SlideData';
 import './Main.scss';
 
 const Main = () => {
   return (
     <div className="Main">
       <MainVideo />
-      <MainCarousel>
-        {SlideData.map(data => (
-          <div key={data.id} className="carouselContainer">
-            <img
-              className="mainCarouselImage"
-              src={data.image}
-              alt={data.name}
-            />
-            <div className="mainCarouselProductName">{data.name}</div>
-            <div className="mainCarouselProductDescription">
-              {data.description}
-            </div>
-          </div>
-        ))}
-      </MainCarousel>
+      <MainCarousel />
       <div className="MainVitamin">a</div>
       <div className="MainShampooIntro">a</div>
       <div className="MainShampooModal">a</div>
--- src/pages/Main/MainCarousel.js
+++ src/pages/Main/MainCarousel.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SlideData from './SlideData';
 import './MainCarousel.scss';
 
-const MainCarousel = ({ children }) => {
-  console.log(children);
+const MainCarousel = () => {
+  const [slidePosition, setSlidePosition] = useState(0);
+
+  const prevSlideHandler = () => {
+    let newPosition = slidePosition;
+    if (newPosition > 0) {
+      newPosition = newPosition - 1;
+    }
+  };
+
+  const nextSlideHanler = () => {
+    let newPosition = slidePosition;
+    if (newPosition < )
+  };
   return (
     <div className="MainCarousel">
-      <div className="carouselContainer">{children}</div>
+      <div className="carouselContainer">
+        {SlideData.map(data => (
+          <div key={data.id} className="mainCarouselContainer">
+            <img
+              className="mainCarouselImage"
+              src={data.image}
+              alt={data.name}
+            />
+            <div className="mainCarouselProductName">{data.name}</div>
+            <div className="mainCarouselProductDescription">
+              {data.description}
+            </div>
+          </div>
+        ))}
+      </div>
       <div className="leftArrowContainer">
         <img
           className="leftArrow"
