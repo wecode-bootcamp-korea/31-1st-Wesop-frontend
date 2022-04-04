152551e14b49a0ba198b3153e653c6e9aaa35428
unknown <chkevi@gmail.com>
2022-04-04 11:20:56 +0900

    Add: Nav 메뉴 WIP

---
+++ src/components/Nav/NavMenu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NavMenu = () => {
+  return (
+    <div className="NavMenu">
+      <div className="menuLogo">Wesop</div>
+      <div className="column"></div>
+    </div>
+  );
+};
