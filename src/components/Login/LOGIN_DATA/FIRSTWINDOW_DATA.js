617330bec714dcc315984cf42bcaf12376eb41ee
Minju <minju1009@gmail.com>
2022-04-05 16:17:01 +0900

    Fix : 지현님이 작업하신 Login 컴포넌틔 임의로 반영해본 파일 삭제

--- src/components/Login/LOGIN_DATA/FIRSTWINDOW_DATA.js
+++
@@ -1,5 +0,0 @@
-const FIRSTWINDOW_DATA = [
-  { infoType: 'email', inputType: 'email', string: '이메일 주소' },
-];
-
-export default FIRSTWINDOW_DATA;
--- src/components/Login/LOGIN_DATA/SIGNIN_DATA.js
+++
@@ -1,6 +0,0 @@
-const SIGNIN_DATA = [
-  { infoType: 'email', inputType: 'email', string: '이메일 주소' },
-  { infoType: 'password', inputType: 'password', string: '패스워드' },
-];
-
-export default SIGNIN_DATA;
