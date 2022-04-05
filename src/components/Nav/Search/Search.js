0fdb5ee6eda07aaff1626b3e682d6e11edb53df1
unknown <chkevi@gmail.com>
2022-04-05 22:39:35 +0900

    WIP

--- src/components/Nav/Search/Search.js
+++ src/components/Nav/Search/Search.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import SearchList from './SearchList';
 import './Search.scss';
 
 const Search = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [data, setData] = useState({});
+  const [searchInput, setSearchInput] = useState('');
+
+  useEffect(() => {
+    fetch('https://node-pagnation.herokuapp.com/users')
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+
+  const inputHandler = e => {
+    setSearchInput(e.target.value);
+  };
+
+  useEffect(() => {
+    const queryString = `?searchMenu=open&search=${searchInput}`;
+    navigate(queryString);
+  }, [searchInput]);
+
+  console.log(location.search);
+
   return (
-    <div className="Search">
+    <div className="search">
       <div className="searchBox">
         <img className="logo" src="/images/common/Wesop.png" alt="로고" />
-        <form className="searchInputWrapper">
-          <input className="searchInput" name="searchInput" type="text" />
+        <div className="searchInputWrapper">
+          <input
+            className="searchInput"
+            name="searchInput"
+            type="text"
+            onChange={inputHandler}
+          />
           <button className="searchBtn">→</button>
-        </form>
+        </div>
       </div>
-      <div className="searchList">a</div>
-      <div className="searchImage">a</div>
+      <div className="searchList">
+        <SearchList />
+      </div>
+      <div className="searchImage">이미지</div>
     </div>
   );
 };
---
+++ src/components/Nav/Search/SearchList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SearchList.scss';
+
+const SearchList = () => {
+  return (
+    <div className="searchList">
+      <div className="listTitle">구매하기</div>
+      <ul className="searchListWrapper">
+        <li className="searchItem">클렌저</li>
+        <li className="searchItem">클렌저</li>
+        <li className="searchItem">클렌저</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SearchList;
