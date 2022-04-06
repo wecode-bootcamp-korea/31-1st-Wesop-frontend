f4bf4b14e64f261441ceb94e5a07e9f816c188d1
Minju <minju1009@gmail.com>
2022-04-06 15:02:36 +0900

    Fix : config.js부분 반영 후 TODO로 주석처리

--- src/config/config.js
+++ src/config/config.js
@@ -6,14 +6,14 @@ export const LOGIN_SERVER_ADDRESS = {
   loginSignUpAddress: `${loginServerRootAddress}/users/signup`,
 };
 
-TODO: 각자 테스트한 후, 최종적으로 아래와 같이 변경
-const BASE_URL = 'http://10.57.2.129:8000';
-const API = {
-  loginMainAddress: `${BASE_URL}/users/check`,
-  loginSignInAddress: `${BASE_URL}/users/login`,
-  loginSignUpAddress: `${BASE_URL}/users/signup`,
-  categoryInfo: `${BASE_URL}/products/categories`,
-  products: `${BASE_URL}/products`,
-};
+// TODO: 각자 테스트한 후, 최종적으로 아래와 같이 변경
+// const BASE_URL = 'http://10.57.2.129:8000';
+// const API = {
+//   loginMainAddress: `${BASE_URL}/users/check`,
+//   loginSignInAddress: `${BASE_URL}/users/login`,
+//   loginSignUpAddress: `${BASE_URL}/users/signup`,
+//   categoryInfo: `${BASE_URL}/products/categories`,
+//   products: `${BASE_URL}/products`,
+// };
 
-export default API;
+// export default API;
