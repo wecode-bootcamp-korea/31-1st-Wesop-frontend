47c7b75e7680a2f5afe7eddcd3294d1f163264b3
전지현 <taxtocode@gmail.com>
2022-04-06 17:48:32 +0900

    Conflict 해결2

--- src/config/config.js
+++ src/config/config.js
@@ -6,16 +6,15 @@ export const LOGIN_SERVER_ADDRESS = {
   loginSignUpAddress: `${loginServerRootAddress}/users/signup`,
 };
 
-<<<<<<< HEAD
 const cartServerRootAddress = 'http://10.58.2.34:8000';
 
 export const CART_SERVER_ADDRESS = {
   cartMainAddress: `${cartServerRootAddress}/carts/cart`,
-=======
+};
+
 const DetailServerRootAddress = 'http://10.58.5.254:8000';
 
 export const DETAIL_SERVER_ADDRESS = {
   mainDescription: `${DetailServerRootAddress}/products/detail/1`,
   bottomDescription: `${DetailServerRootAddress}/products/recommend/1`,
->>>>>>> master
 };
