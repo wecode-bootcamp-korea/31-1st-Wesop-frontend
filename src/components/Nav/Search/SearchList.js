992549d5a8618c4c8932294e84dc22abebb1645e
unknown <chkevi@gmail.com>
2022-04-06 12:55:09 +0900

    WIP

--- src/components/Nav/Search/SearchList.js
+++ src/components/Nav/Search/SearchList.js
@@ -1,3 +1,4 @@
+import { hover } from '@testing-library/user-event/dist/hover';
 import React, { useState } from 'react';
 import SearchImage from './SearchImage';
 import './SearchList.scss';
@@ -5,15 +6,15 @@ import './SearchList.scss';
 const SearchList = () => {
   const [listHover, setListHover] = useState(false);
 
-  const hoverHandler = () => {
-    setListHover(true);
+  const hoverHandler = boolean => {
+    setListHover(boolean);
   };
   return (
-    <div className="searchList">
+    <div className="searchList" onMouseLeave={() => hoverHandler(false)}>
       <div className="listContainer">
         <div className="listTitle">구매하기</div>
         <ul className="searchListWrapper">
-          <li className="searchItem" onMouseOver={hoverHandler}>
+          <li className="searchItem" onMouseOver={() => hoverHandler(true)}>
             클렌저
           </li>
           <li className="searchItem">클렌저</li>
