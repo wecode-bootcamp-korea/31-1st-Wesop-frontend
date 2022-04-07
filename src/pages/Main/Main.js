6a2230daacf9eb2ce752ada3378df8ef7346f0ed
unknown <chkevi@gmail.com>
2022-04-07 13:20:09 +0900

    Modify: push전 수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,18 +1,7 @@
 import React from 'react';
-// import Footer from '../../components/Footer/Footer';
-import Nav from '../../components/Nav/Nav';
-import NavMenu from '../../components/Nav/Menu/NavMenu';
-import Search from '../../components/Nav/Search/Search';
 
 const Main = () => {
-  return (
-    <div className="Main">
-      <Nav />
-      {/* <Search /> */}
-      <NavMenu />
-      {/* <Footer /> */}
-    </div>
-  );
+  return <div className="Main" />;
 };
 
 export default Main;
